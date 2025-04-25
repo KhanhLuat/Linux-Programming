2a4100e6fe1a2ed3d419cbc71abf96de2348cee1
KhanhLuat <nguyenkhanhluat123@gmail.com>
2025-04-25 22:36:21 +0700

    luat

--- 01_General/blinkled.c
+++ 01_General/blinkled.c
@@ -9,19 +9,18 @@
 //#include <unistd.h>
 
 int main() {
-	FILE * trigger[4] = {fopen("/sys/class/leds/beaglebone:green:usr3/trigger", "w"), fopen("/sys/class/leds/beaglebone:green:usr2/trigger", "w"),
-						fopen("/sys/class/leds/beaglebone:green:usr1/trigger", "w"), fopen("/sys/class/leds/beaglebone:green:usr0/trigger", "w")};
-	FILE * brightness[4] = {fopen("/sys/class/leds/beaglebone:green:usr3/brightness", "w"),fopen("/sys/class/leds/beaglebone:green:usr2/brightness", "w"),
-							fopen("/sys/class/leds/beaglebone:green:usr1/brightness", "w"),fopen("/sys/class/leds/beaglebone:green:usr0/brightness", "w"),};
+	FILE * trigger[3] = {fopen("/sys/class/leds/beaglebone:green:usr3/trigger", "w"), fopen("/sys/class/leds/beaglebone:green:usr2/trigger", "w"),
+						fopen("/sys/class/leds/beaglebone:green:usr1/trigger", "w")};
+	FILE * brightness[3] = {fopen("/sys/class/leds/beaglebone:green:usr3/brightness", "w"),fopen("/sys/class/leds/beaglebone:green:usr2/brightness", "w"),
+							fopen("/sys/class/leds/beaglebone:green:usr1/brightness", "w")};
 	int on = 0;
-	for(int i =0; i < 4; i++){
+	for(int i =0; i < 3; i++){
 		fprintf(trigger[0], "none\n");
 		fprintf(trigger[1], "none\n");
 		fprintf(trigger[2], "none\n");
-		fprintf(trigger[3], "none\n");
 	}
 	while(1) {
-		for ( int i = 0; i < 4; i++){
+		for ( int i = 0; i < 3; i++){
 			fprintf(brightness[i], "%d\n", on);
 			fflush(brightness[i]);
 		}
