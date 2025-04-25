70bc1f80967b2f66a6845cb6b975dfbbcac6a56d
KhanhLuat <nguyenkhanhluat123@gmail.com>
2025-04-25 22:14:11 +0700

    add

--- 01_General/blinkled.c
+++ 01_General/blinkled.c
@@ -1,26 +1,34 @@
-
-
-
+////////////////////////////////////////
+//	blinkLED.c
+//      Blinks the USR3 LED.
+//	Wiring:	None
+//	Setup:	
+//	See:	
+////////////////////////////////////////
 #include <stdio.h>
-#include <unistd.h>
+//#include <unistd.h>
 
-int main(){
-	FILE *trigger = fopen("/sys/class/leds/beaglebone:green:usr0/trigger","w");
-	FILE *brightness = fopen("/sys/class/leds/beaglebone:green:usr0/brightness","w");
-
+int main() {
+	FILE * trigger[4] = {fopen("/sys/class/leds/beaglebone:green:usr3/trigger", "w"), fopen("/sys/class/leds/beaglebone:green:usr2/trigger", "w"),
+						fopen("/sys/class/leds/beaglebone:green:usr1/trigger", "w"), fopen("/sys/class/leds/beaglebone:green:usr0/trigger", "w")};
+	FILE * brightness[4] = {fopen("/sys/class/leds/beaglebone:green:usr3/brightness", "w"),fopen("/sys/class/leds/beaglebone:green:usr2/brightness", "w"),
+							fopen("/sys/class/leds/beaglebone:green:usr1/brightness", "w"),fopen("/sys/class/leds/beaglebone:green:usr0/brightness", "w"),};
 	int on = 0;
-	fprintf(trigger, "none\n");
-
-	while(1){
-		fprintf(brightness, "%d\n",on);
-		fflush(brightness);
-		if(!on){
-			on = 1;
-		}else{
-			on = 0;
-		}
-		usleep(50000);
+	for(int i =0; i < 4; i++){
+		fprintf(trigger[0], "none\n");
+		fprintf(trigger[1], "none\n");
+		fprintf(trigger[2], "none\n");
+		fprintf(trigger[3], "none\n");
 	}
-
-
+	while(1) {
+		for ( int i = 0; i < 4; i++){
+			fprintf(brightness[i], "%d\n", on);
+			fflush(brightness[i]);
+		}
+		if(!on) 
+			on = 1;
+		else 
+			on = 0;
+			usleep(500000);
+	}
 }
