a7b275766edfe3280e6a1742a71eaae2fb62a50c
KhanhLuat <nguyenkhanhluat123@gmail.com>
2025-04-27 20:43:06 +0700

    fix8

--- 01_General/src/CheckFolderBrightness.c
+++ 01_General/src/CheckFolderBrightness.c
@@ -2,7 +2,7 @@
 #include "CheckFolderBrightness.h"
 int CheckFolderBrightness(FILE *ptr){
     if(ptr != NULL){
-        printf("Open the file path");
+        printf("Ok\n");
         return 1;
     }else{
         printf("Cannot open the file path");
