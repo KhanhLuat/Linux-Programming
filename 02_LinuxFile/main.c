7c3673c13498785ab3e8fd389fb2f06818393e4c
KhanhLuat <nguyenkhanhluat123@gmail.com>
2025-04-28 21:39:02 +0700

    file

---
+++ 02_LinuxFile/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main(void){
+    int fd;
+    int numb_read, numb_write;
+    char buf1[12] = "hello world\n";
+
+    fd = open("hello.txt", O_RDWR | O_CREAT, 0665);
+    if(-1 == fd){
+        printf("open() hello.txt failed\n");
+    }
+
+    numb_write = write(fd, buf1, strlen(buf1));
+    printf("Write %d bytes to hello.txt\n", numb_write);
+
+    //lseek(fd, 2, SEEK_SET);
+    write(fd, "AAAAAA",strlen("AAAAAA"));
+
+    close(fd);
+
+    return 0;
+}
